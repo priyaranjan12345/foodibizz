71fdcdebda58d637ea86d64324053d80a33a1458
Gnyan-Coder <gnyanranjanbehera@gmail.com>
2023-09-06 10:02:55 +0530

    checkout

--- lib/src/features/dashboard/data/repository/dashboard_repository.dart
+++ lib/src/features/dashboard/data/repository/dashboard_repository.dart
@@ -1,11 +1,10 @@
 import 'package:dio/dio.dart';
-import 'package:foodibizz/data/apis/dashboard/i_dashboard_api.dart';
+
 import 'package:foodibizz/global/exceptions/no_internet_exception.dart';
 import 'package:foodibizz/src/features/dashboard/data/services/i_dashboard_api.dart';
 import 'package:foodibizz/src/features/dashboard/model/all_food_items_response.dart';
 import 'package:multiple_result/multiple_result.dart';
 
-import 'package:foodibizz/data/model/dashboard_model/all_food_items_response.dart';
 import 'package:foodibizz/global/exceptions/base_exception.dart';
 import 'i_dashboard_repository.dart';
 
