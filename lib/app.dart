37f99219629f3f2d94327c58628b703ad7604cfd
Priyaranjan <priyaranjanmantri@gmail.com>
2023-09-13 23:25:08 +0530

    ✨ app stagging added

--- lib/app.dart
+++ lib/app.dart
@@ -1,34 +1,17 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-import 'global/api_client/base_url_pod.dart';
-import 'src/core/locale_storage/app_storage_pod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'src/core/localization/l10n.dart';
 import 'src/core/localization/locale_pod.dart';
 import 'src/core/routes/auto_route_observer.dart';
 import 'src/core/routes/router_pod.dart';
 import 'src/core/theme/app_them_pod.dart';
 import 'src/core/theme/app_themes.dart';
-import 'src/core/localization/l10n.dart';
 import 'src/core/utils/responsive_break_point_wrapper.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  final appBox = await Hive.openBox('appBox');
-  runApp(
-    ProviderScope(
-      overrides: [
-        baseUrlProvider.overrideWith((ref) => "my base url"),
-        appBoxProvider.overrideWithValue(appBox),
-      ],
-      child: const MyApp(),
-    ),
-  );
-}
-
-class MyApp extends ConsumerWidget{
-  const MyApp({super.key});
+class App extends ConsumerWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -51,7 +34,8 @@ class MyApp extends ConsumerWidget{
         ],
       ),
       builder: (context, child) {
-        child = ResponsiveBreakPointWrapper(child: child!);
+        child = DevicePreview.appBuilder(context, child);
+        child = ResponsiveBreakPointWrapper(child: child);
         return child;
       },
     );
