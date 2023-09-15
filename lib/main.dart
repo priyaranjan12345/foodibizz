1432519d9200bccbf515587878c1640504113a52
Gnyan-Coder <gnyanranjanbehera@gmail.com>
2023-09-15 12:36:34 +0530

    adding main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'global/api_client/base_url_pod.dart';
+import 'app.dart';
+import 'src/core/locale_storage/app_storage_pod.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  final appBox = await Hive.openBox('appBox');
+  runApp(
+    ProviderScope(
+      overrides: [
+        baseUrlProvider.overrideWith((ref) => "http://3.27.90.34:8000/"),
+        appBoxProvider.overrideWithValue(appBox),
+      ],
+      child: const App(),
+    ),
+  );
+}
