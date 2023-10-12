4028c1437837497a054c09398115c9e99c613271
Priyaranjan <priyaranjanmantri@gmail.com>
2023-10-12 14:47:41 +0530

    🎉 bottomNav route

--- lib/src/features/home/home_screen.dart
+++ lib/src/features/home/home_screen.dart
@@ -4,41 +4,66 @@ import 'package:foodibizz/src/core/routes/app_routes.gr.dart';
 import 'package:foodibizz/src/core/localization/l10n.dart';
 
 @RoutePage(deferredLoading: true, name: "HomeRoute")
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ///add here
+  List<int> history = [];
+  Future<bool> _onWillPop(TabsRouter tabsRouter) async {
+    //modify
+    if (history.isNotEmpty) {
+      tabsRouter.setActiveIndex(history.last);
+      history.removeLast();
+      return false;
+    } else {
+      return true;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    return AutoTabsScaffold(
+
+    return AutoTabsRouter.pageView(
       routes: const [
         DashboardRoute(),
         HistoryRoute(),
         SettingsRoute(),
       ],
-      bottomNavigationBuilder: (context, tabsRouter) {
-        final tabsRouter = AutoTabsRouter.of(context);
-
-        return NavigationBar(
-          labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-          selectedIndex: tabsRouter.activeIndex,
-          onDestinationSelected: (int index) {
-            tabsRouter.setActiveIndex(index);
-          },
-          destinations: [
-            NavigationDestination(
-              icon: const Icon(Icons.dashboard_outlined),
-              label: l10n.dashboard,
+      builder: (ctx, child, pageController) {
+        final tabsRouter = AutoTabsRouter.of(ctx);
+        return WillPopScope(
+          onWillPop: () => _onWillPop(tabsRouter),
+          child: Scaffold(
+            body: child,
+            bottomNavigationBar: NavigationBar(
+              labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+              selectedIndex: tabsRouter.activeIndex,
+              onDestinationSelected: (int index) {
+                history.add(tabsRouter.activeIndex);
+                tabsRouter.setActiveIndex(index);
+              },
+              destinations: [
+                NavigationDestination(
+                  icon: const Icon(Icons.dashboard_outlined),
+                  label: l10n.dashboard,
+                ),
+                NavigationDestination(
+                  icon: const Icon(Icons.history),
+                  label: l10n.history,
+                ),
+                NavigationDestination(
+                  icon: const Icon(Icons.settings),
+                  label: l10n.setting,
+                ),
+              ],
             ),
-            NavigationDestination(
-              icon: const Icon(Icons.history),
-              label: l10n.history,
-            ),
-            NavigationDestination(
-              icon: const Icon(Icons.settings),
-              label: l10n.setting,
-            ),
-          ],
+          ),
         );
       },
     );
